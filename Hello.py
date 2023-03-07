d9436ab74c4691cb7fd10e07071caa05aa59a996
Akechikara <124995407+Akechikara@users.noreply.github.com>
2023-03-07 11:03:00 +0700

    แก้ไข hello และ อัพโหลดโฟลเดอร์ basic package

---
+++ Hello.py
@@ -0,0 +1,10 @@
+print('Hello Peche')
+
+print('Hello Looknam')
+
+print('Hello Github')
+
+friend = ['Loong','Pa Lek','Loong Dam']
+
+print(friend[0])
+
---
+++ basic_package/info.py
@@ -0,0 +1,2 @@
+name = 'Peche'
+lastname = 'Pratoomraj'
